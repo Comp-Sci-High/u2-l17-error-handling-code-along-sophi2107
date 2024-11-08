d4ea7c1748ae59aa986947b4983e1d9b1077abb2
sophi2107 <sophia.rivera25@compscihigh.org>
2024-11-08 15:28:34 +0000

    done

--- index.js
+++ index.js
@@ -7,24 +7,32 @@
 // Step 3: Create a .gitignore file and add .env to it
 
 // Step 4: Import dotenv config
-
+require('dotenv').config()
 // Step 5: Access the API key from the .env file using process.env and store it in a variable
-
+const apiKey = process.env.API_KEY
 // Step 6: Construct the request URL using the environmental variable
-const requestURL = "https://api.nasa.gov/planetary/apod?api_key=";
+const requestURL = "https://api.nasa.gov/planetary/apod?api_key=" + apiKey;
 
+console.log(requestURL)
 async function fetchData(url) {
   // Step 7: Wrap your code in a try {} block
+  try{
   const response = await fetch(url);
 
   // Step 8: Check if the response status is not OK
   // Log an error message if the response status is not OK
+if (response.ok ===false) {
+  console.log(response.status)
+}
+
 
   const data = await response.json();
 
   console.log(data);
   return data;
-
+  }catch(error){
+    console.log(error.message)
+  }
   // Step 10: Add a catch block to handle any errors that occur during the fetch or data processing
 }
 
